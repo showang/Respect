95673e085b1f517139697b721b480d3bccd3d946
William Wang <showang730@gmail.com>
2018-11-15 15:14:10 +0800

    Fix error handling issues.

--- respect/src/test/java/me/showang/respect/RestFulApiTest.kt
+++ respect/src/test/java/me/showang/respect/RestFulApiTest.kt
@@ -3,7 +3,9 @@ package me.showang.respect
 import kotlinx.coroutines.runBlocking
 import me.showang.respect.core.ContentType
 import me.showang.respect.core.HttpMethod
+import me.showang.respect.core.ParseError
 import org.junit.Test
+import java.lang.IllegalArgumentException
 
 class RestFulApiTest {
 
@@ -81,6 +83,32 @@ class RestFulApiTest {
             get() = ContentType.JSON
         override val body: ByteArray
             get() = "{\"id\":\"$id\"}".toByteArray()
+    }
+
+
+    class ParseErrorApi : RespectApi<String>() {
+        override val httpMethod: HttpMethod
+            get() = HttpMethod.GET
+        override val url: String
+            get() = "https://jsonplaceholder.typicode.com/posts/1/comments"
+        override val contentType: String
+            get() = ContentType.JSON
+
+        override fun parse(bytes: ByteArray): String {
+            throw IllegalArgumentException("Parse Error")
+        }
+    }
+
+    @Test
+    fun testError_parseException() {
+        runBlocking {
+            ParseErrorApi().start(this, {
+                println("on parse error: $it")
+                assert(it is ParseError)
+            }) {
+                assert(false)
+            }
+        }
     }
 
 }
