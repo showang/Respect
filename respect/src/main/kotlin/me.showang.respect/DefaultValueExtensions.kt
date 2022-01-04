daaaed3529dd137dbcfd9d7b57612375b7911548
Hsuanhao <hsuanhao.wang@shopback.com>
2022-01-04 17:43:38 +0800

    [Release v1.1.0] Supports cancellation request

--- respect/src/main/kotlin/me.showang.respect/DefaultValueExtensions.kt
+++ respect/src/main/kotlin/me.showang.respect/DefaultValueExtensions.kt
@@ -1,5 +1,6 @@
 package me.showang.respect
 
+import kotlinx.coroutines.CancellationException
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers.IO
 import kotlinx.coroutines.Dispatchers.Main
@@ -7,9 +8,12 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 import me.showang.respect.core.RequestExecutor
 import me.showang.respect.okhttp.OkhttpRequestExecutor
+import okhttp3.OkHttpClient
 import kotlin.coroutines.CoroutineContext
 
-private val defaultExecutor: RequestExecutor by lazy { OkhttpRequestExecutor() }
+private val defaultExecutor: RequestExecutor by lazy {
+    OkhttpRequestExecutor(OkHttpClient())
+}
 
 class Respect {
     companion object {
@@ -18,12 +22,19 @@ class Respect {
 }
 
 suspend fun <Result> RestfulApi<Result>.request(): Result =
-    request(Respect.specifiedExecutor ?: defaultExecutor)
+        request(Respect.specifiedExecutor ?: defaultExecutor)
+
+suspend fun <Result> RestfulApi<Result>.request(errorDelegate: (Throwable) -> Unit): Result? = try {
+    request()
+} catch (e: Throwable) {
+    e.takeIf { it !is CancellationException }?.let(errorDelegate)
+    null
+}
 
 fun <Result> RestfulApi<Result>.start(
-    uiContext: CoroutineContext,
-    failHandler: (Throwable) -> Unit = {},
-    successHandler: (Result) -> Unit
+        uiContext: CoroutineContext,
+        failHandler: (Throwable) -> Unit = {},
+        successHandler: (Result) -> Unit
 ) = CoroutineScope(IO).launch {
     try {
         val result = request()
@@ -34,6 +45,6 @@ fun <Result> RestfulApi<Result>.start(
 }
 
 fun <Result> RestfulApi<Result>.start(
-    failHandler: (Throwable) -> Unit = {},
-    successHandler: (Result) -> Unit
+        failHandler: (Throwable) -> Unit = {},
+        successHandler: (Result) -> Unit
 ) = start(Main, failHandler, successHandler)
